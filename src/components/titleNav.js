de4bc2820bb04e942364ad14c3db86ed9e07fced
Kip <Kipling.Dunlap@gmail.com>
2023-03-26 21:41:52 -0400

    cosmetic changes

--- src/components/titleNav.js
+++ src/components/titleNav.js
@@ -5,14 +5,14 @@ function titleNav() {
         <div className='s12 m12 l12'>
           <h1>Kipling C. Dunlap</h1>
           <ul className="contact tabs black-text center-align">
-            <li className="tab hoverable"><a href="tel:+14045906275">404.590.6275</a></li>
-            <li className="tab hoverable"><a href="mailto:Kip@KipDunlap.me">Kip@KipDunlap.me</a></li>
-            <li className="tab hoverable"><a href="https://github.com/UrbaneD0ge" target="_blank" rel="noreferrer">Github</a></li>
-            <li className="tab hoverable"><a href="https://www.linkedin.com/in/kiplingdunlap/"
+            <li className="tab"><a href="tel:+14045906275">404.590.6275</a></li>
+            <li className="tab"><a href="mailto:Kip@KipDunlap.me">Kip@KipDunlap.me</a></li>
+            <li className="tab"><a href="https://github.com/UrbaneD0ge" target="_blank" rel="noreferrer">Github</a></li>
+            <li className="tab"><a href="https://www.linkedin.com/in/kiplingdunlap/"
               target="_blank" rel="noreferrer">LinkedIn</a>
             </li>
-            <li className="tab hoverable"><a href="https://twitter.com/UrbaneDoge" target="_blank" rel="noreferrer">Twitter</a></li>
-            <li className="tab hoverable"><a href="https://open.spotify.com/user/pirateonptree"
+            <li className="tab"><a href="https://twitter.com/UrbaneDoge" target="_blank" rel="noreferrer">Twitter</a></li>
+            <li className="tab"><a href="https://open.spotify.com/user/pirateonptree"
               target="_blank" rel="noreferrer">Spotify</a>
             </li>
           </ul>
