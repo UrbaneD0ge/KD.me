7276c5f6adf03263a98c20bf951d7b1a038ff62c
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2024-10-29 18:57:36 -0400

    gotta install it

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
